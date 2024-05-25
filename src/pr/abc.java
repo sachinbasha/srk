b2c62ad68725abf7766412129aadbfde8f217493
sachinbasha <uchesachin96@gmail.com>
2024-05-25 10:46:25 +0530

    hi boss

--- src/pr/abc.java
+++ src/pr/abc.java
@@ -3,8 +3,14 @@ package pr;
 public class abc {
 
 	public static void main(String[] args) {
-		System.out.println("sachin");
+		System.out.println("sachin2");
 
 	}
+	public static void main() {
+		System.out.println("sachin2");
+
+	}
+	
+	
 
 }
